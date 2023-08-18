1c8bbfc44dd79777bef1714254c2adc5cb4fd750
hosseinhtt <hossein.taybi@yahoo.com>
2023-08-18 20:43:40 +0330

    all models got created and added to django admin in branch models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=25)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Post(BaseModel):
+    title = models.CharField(
+        max_length=50, help_text='Enter the title', default='post-number')
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author}: {self.title=}- {self.content[:15]}'
+
+
+class Comment(BaseModel):
+    name = models.CharField(max_length=20)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}: {self.content[:10]}...'
