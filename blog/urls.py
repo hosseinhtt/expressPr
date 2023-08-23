3cbdd1f4517eab95e9befcfb8a10f26c900a8e13
hosseinhtt <hossein.taybi@yahoo.com>
2023-08-23 16:57:47 +0330

    session 7 is done

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('post', PostListView.as_view(), name='post-list'),
     path('post/<slug>', PostDetailView.as_view(), name='post-detail'),
+    path('api/post-list', PostListAPIView.as_view(), name='post-api'),
 ]
